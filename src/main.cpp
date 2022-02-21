f0613f61378839a5a7d2ec780ed1bb3f9cd2f06f
NickStrupat <nick.strupat@gmail.com>
2022-02-21 04:53:36 -0500

    Add CLI parser library and read the project name and version from vcpkg.json.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <magic_enum.hpp>
 #include <nlohmann/json.hpp>
+#include <CLI/CLI.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(int argc, char** argv) {
+    CLI::App app{std::string(EXECUTABLE_NAME) + " compiler - version " + EXECUTABLE_VERSION, EXECUTABLE_NAME};
+
+    app.add_flag("-v,--version", [](std::int64_t i){cout << EXECUTABLE_VERSION << endl;}, "Print version information");
+
+    if (argc <= 1)
+        return app.exit(CLI::CallForHelp());
+    try {
+        app.parse(argc, argv);
+    }
+    catch (CLI::ParseError const & e) {
+        return app.exit(e);
+    }
     return 0;
 }
