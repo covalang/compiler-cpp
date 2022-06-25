f1c57a892a76d659a0d2647864e47e5dee038180
NickStrupat <nick.strupat@gmail.com>
2022-06-25 19:04:57 -0400

    Update to ANTLR 4.10.1.

--- src/grammar/generate.ps1
+++ src/grammar/generate.ps1
@@ -1,7 +1,7 @@
 $location = Get-Location
 try {
     Set-Location $PSScriptRoot
-    java -jar antlr-4.9.3-complete.jar -Dlanguage=Cpp CovaLexer.g4 CovaParser.g4 -o generated
+    java -jar antlr-4.10.1-complete.jar -Dlanguage=Cpp CovaLexer.g4 CovaParser.g4 -o generated
 }
 finally {
     Set-Location $location
