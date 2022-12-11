d8c8e7a10b12ea005fdd9ea2a693336c0f0c498f
NickStrupat <nick.strupat@gmail.com>
2022-12-10 19:53:05 -0500

    Upgrade vcpkg to the 2022.11.14 release. This includes an upgrade of ANTLR to 4.11.1.

--- src/grammar/generate.ps1
+++ src/grammar/generate.ps1
@@ -1,8 +1,8 @@
 $location = Get-Location
 try {
-    Set-Location $PSScriptRoot
-    java -jar antlr-4.10.1-complete.jar -Dlanguage=Cpp CovaLexer.g4 CovaParser.g4 -o generated
+	Set-Location $PSScriptRoot
+	java -jar antlr-4.11.1-complete.jar -Dlanguage=Cpp CovaLexer.g4 CovaParser.g4 -o generated
 }
 finally {
-    Set-Location $location
+	Set-Location $location
 }
